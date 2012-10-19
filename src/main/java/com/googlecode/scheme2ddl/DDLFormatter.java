2d251a958dd289dc0b87f2ebc237a3f23f7c8c83
resheto@gmail.com <resheto@gmail.com@ee54ffa5-7344-276b-3431-3339cd9c5f46>
2012-10-19 11:16:34 +0000

    partial work

--- src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
+++ src/main/java/com/googlecode/scheme2ddl/DDLFormatter.java
@@ -1,23 +1,21 @@
 package com.googlecode.scheme2ddl;
 
+import com.googlecode.scheme2ddl.domain.UserObject;
+
 /**
- * User: Reshetnikov AV resheto@gmail.com
- * Date: 10.03.11
- * Time: 11:04
+ * @author A_Reshetnikov
+ * @since Date: 18.10.2012
  */
 public class DDLFormatter {
 
+    private Boolean noFormat;
+    private Boolean statementOnNewLine;
 
-    private Boolean noFormat ;
-    private Boolean statementOnNewLine ;
-  //  private Boolean removeLastSlash;
+    public String formatDDL(String ddl) {
+        if (noFormat) return ddl;
 
 
-    public void formatDDL(UserObject obj){
-        if (noFormat) return;
-
-        String ddl = obj.getDdl();
-        if (statementOnNewLine){
+        if (statementOnNewLine) {
             // Get a new line specific to the system
             String newline = System.getProperty("line.separator");
 
@@ -26,36 +24,14 @@ public class DDLFormatter {
             ddl = ddl.replace(";COMMENT", ";" + newline + "COMMENT");
             ddl = ddl.replace(";CREATE", ";" + newline + "CREATE");
         }
-
-//        if (removeLastSlash){
-//            if (ddl.endsWith("/")){
-//                ddl = ddl.substring(0, ddl.lastIndexOf("/"));
-//            }
-//        }
-        obj.setDdl(ddl);
-    }
-
-    public Boolean getStatementOnNewLine() {
-        return statementOnNewLine;
-    }
-
-    public void setStatementOnNewLine(Boolean statementOnNewLine) {
-        this.statementOnNewLine = statementOnNewLine;
-    }
-
-//    public Boolean getRemoveLastSlash() {
-//        return removeLastSlash;
-//    }
-//
-//    public void setRemoveLastSlash(Boolean removeLastSlash) {
-//        this.removeLastSlash = removeLastSlash;
-//    }
-
-    public Boolean getNoFormat() {
-        return noFormat;
+        return ddl;
     }
 
     public void setNoFormat(Boolean noFormat) {
         this.noFormat = noFormat;
+    }
+
+    public void setStatementOnNewLine(Boolean statementOnNewLine) {
+        this.statementOnNewLine = statementOnNewLine;
     }
 }
